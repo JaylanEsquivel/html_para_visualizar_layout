446a4ac2e3f238eb3b1fe27e84ddf7be4b56ca8d
JaylanEsquivel <jaylan1997@hotmail.com>
2018-07-10 10:20:06 -0300

    Commit Unico

---
+++ index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Oxente Menina - Pizza</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <style>
+            body{
+                background: url('pizza_na_pedra_layout_site_HR.png');
+                background-repeat: no-repeat;
+                background-position: top center;
+                background-attachment: scroll;
+                background-size: 100%;
+                height: 3565px;
+                
+            }
+            .redes-sociais {
+                width: 42px;
+                background-color: #c8c6c6;
+                opacity: 0.5;
+                padding: 12px 8px;
+                position: fixed;
+                top: 48%;
+                left: 100%;
+                margin-left: -57px;
+            }
+            .redes-sociais:hover {
+                opacity: 1;
+            }
+            .redes-sociais {
+                left: 0 !important;
+                margin-left: 0 !important;
+            }
+            .redes-sociais .box-container {
+                background-color: #eee;
+                padding: 6px;
+            }
+            .redes-sociais .box-container .item {
+                display: block;
+                width: 30px;
+                height: 25px;
+                background-color: #fb722c;
+                text-align: center;
+                margin-bottom: 6px;
+                padding-top: 5px;
+            }
+            .redes-sociais .box-container .item i {
+                font-size: 20px;
+                color: #eee;
+            }
+        </style>
+    </head>
+    <body>
+<!--        <div class="redes-sociais">
+
+            <div class="box-container">
+
+                    <a href="https://www.facebook.com/transamericapop" class="item" title="Facebook" target="_blank">
+                        <i class="fa fa-facebook"></i>
+                    </a>
+                    <a href="http://instagram.com/transamericapop" class="item" title="Instagram" target="_blank">
+                        <i class="fa fa-instagram"></i>
+                    </a>
+                    <a href="https://twitter.com/transamericapop" class="item" title="Twitter" target="_blank">
+                        <i class="fa fa-twitter"></i>
+                    </a>
+                    <a href="https://www.youtube.com/c/transamericapopsp" class="item" title="YouTube" target="_blank">
+                        <i class="fa fa-youtube"></i>
+                    </a>
+            </div>
+        </div>-->
+    </body>
+</html>
